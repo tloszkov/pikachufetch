06f9ac0f1e2ff2f20331cb13cb9dccdf0d239110
Zsolt Kovacs <kzsolt82@gmail.com>
2023-07-24 14:01:32 +0300

    Some modifications with first tasks!

--- gotta-fetch-em-all/src/App.js
+++ gotta-fetch-em-all/src/App.js
@@ -1,7 +1,31 @@
 import './App.css';
+import React from 'react';
+import {useEffect,useState} from 'react';
 
+
+
+
+const dataFetch = async () => {
+  try {
+    const data = await fetch('https://pokeapi.co/api/v2/location');
+    const fetchedData = await data.json();
+    console.log("fetchedData:", fetchedData.results)
+  } catch (error) {
+    console.log("Error during data fetching:", error);
+    
+  }
+};
 
 function App() {
+
+  const [locations, setLocations] = useState([]);
+  
+  useEffect(() => {
+
+    setLocations(dataFetch());
+    console.log("Locations", locations);
+  }, []);
+
   return (
     <div className="App">
       <div>Gotta fetch em all!</div>
