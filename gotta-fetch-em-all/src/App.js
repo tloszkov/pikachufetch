d0cfc24633daf16d90fc52a789638695d4d9b77c
Zsolt Kovacs <kzsolt82@gmail.com>
2023-07-24 13:39:47 +0300

    Basic code created!

---
+++ gotta-fetch-em-all/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>Gotta fetch em all!</div>
+      <div>Try!</div>
+    </div>
+  );
+}
+
+export default App;
